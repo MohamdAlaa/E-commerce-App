9ad48e4fa14f75b04c8eee359e68fc3c78aa3a94
MohamdAlaa <mohamedalaaelmenyawe606@gmail.com>
2024-06-12 14:27:26 +0300

    finished from  search Bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,25 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const SearchBar = () => {
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name") as string;
+
+    if (name) {
+      router.push(`/list?name=${name}`);
+    }
+  };
+
   return (
     <form
       className="flex items-center justify-between flex-1 gap-4 p-2 bg-gray-100 rounded-md"
-      // onSubmit={handleSearch}
+      onSubmit={handleSearch}
     >
       <input
         type="text"
